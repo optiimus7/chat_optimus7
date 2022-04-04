36399194048c0703cfc9b7ee2418d8b554493969
Jefferson Luiz Rossini <jerossinilp@gmail.com>
2022-04-04 14:25:24 -0300

    Chat Finalizado

--- app/Http/Controllers/Chat/ChatController.php
+++ app/Http/Controllers/Chat/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Chat;
 
+use App\Events\Chat\MessageCreated;
 use App\Http\Controllers\Controller;
 use App\Models\Chat\Message;
 use Illuminate\Http\Request;
@@ -39,6 +40,10 @@ class ChatController extends Controller
 
         $message['user'] = $user;
 
+        // enviando a mensagem para todos os usuario logados
+        broadcast(new MessageCreated($message))->toOthers();
+
+        // retornando para o usuario que enviou
         return response()->json($message, 201);
     }
 }
